4cd81e784a49c2d6e27f57095feeffeb1a263ec9
ShockTroopa14 <52359305+ShockTroopa14@users.noreply.github.com>
2021-07-15 18:51:20 -0400

    Revert "Database"
    This reverts commit 183f393d690020f2dec2e48fc4b2129fd0c3d3d6.

--- .history/Server/Config/db_20210715175537.ts
+++
@@ -1,3 +0,0 @@
-export const HostName = "RemoteHost";
-export const Secret = "someSecret";
-export const RemoteURI = "mongodb+srv://chinedu:survey@survey-wizard.aqdd3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
