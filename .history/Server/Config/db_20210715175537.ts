183f393d690020f2dec2e48fc4b2129fd0c3d3d6
chineduoke <83087763+chineduoke@users.noreply.github.com>
2021-07-15 18:02:40 -0400

    Database

---
+++ .history/Server/Config/db_20210715175537.ts
@@ -0,0 +1,3 @@
+export const HostName = "RemoteHost";
+export const Secret = "someSecret";
+export const RemoteURI = "mongodb+srv://chinedu:survey@survey-wizard.aqdd3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
