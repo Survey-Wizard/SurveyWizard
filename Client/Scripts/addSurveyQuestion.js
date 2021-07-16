af190f6d5c99771bf95ec468d028c6830a30a29c
ShockTroopa14 <52359305+ShockTroopa14@users.noreply.github.com>
2021-07-15 22:07:24 -0400

    survey edit and create complete

---
+++ Client/Scripts/addSurveyQuestion.js
@@ -0,0 +1,17 @@
+var totalQuestions = 0;
+
+function handleDisplayQuestions() {
+  var questionContainer = document.getElementById("create-question");
+  var newQuestion = document.createElement("div");
+  newQuestion.setAttribute("class", "question-container");
+  newQuestion.setAttribute("id", `question-${totalQuestions}`);
+  newQuestion.innerHTML = `<h2 class='question-number'>Q${totalQuestions}</h2><input class='question-box'/><select name='surveyType' id='cars'><option value='Survey Category'>Question Type</option><option value='Community or volunteer Feedback'>Multiple Choice</option><option value='Customer Feedback'>Radio Buttons</option> <option value='Concept Production'>Text Box</option> <option value='Employee Engagement'>Star Rating</option></select>`;
+  questionContainer.appendChild(newQuestion);
+
+  totalQuestions++;
+}
+
+// let questionContainer = document.getElementById("create-question");
+//     let newQuestion = `<div class='question-container' id=${i}> <h2 class='question-number'>Q${i}</h2><input class='question-box'/><select name='surveyType' id='cars'><option value='Survey Category'>Question Type</option><option value='Community or volunteer Feedback'>Multiple Choice</option><option value='Customer Feedback'>Radio Buttons</option> <option value='Concept Production'>Text Box</option> <option value='Employee Engagement'>Star Rating</option></select></div>`;
+//     questionContainer.innerHTML = newQuestion;
+//     questionContainer.append(newQuestion);
--- Server/Config/app.ts
+++ Server/Config/app.ts
@@ -12,7 +12,7 @@ export default app;
 // view engine setup
 app.set("views", path.join(__dirname, "../Views"));
 app.set("view engine", "ejs");
-
+  
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
