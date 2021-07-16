d1f1e2003073c998fd782d6de961814dad4686e6
ShockTroopa14 <52359305+ShockTroopa14@users.noreply.github.com>
2021-07-15 22:09:50 -0400

    Revert "Merge branch 'main' of https://github.com/SurveyWizard-comp229/SurveyWizard-Express into main"
    This reverts commit fa2582d5186c4dae2fb0d563ce854f0b3d2430bd, reversing
    changes made to af190f6d5c99771bf95ec468d028c6830a30a29c.

--- Server/Config/db.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RemoteURI = exports.Secret = exports.HostName = void 0;
-exports.HostName = "RemoteHost";
-exports.Secret = "someSecret";
-exports.RemoteURI = "mongodb+srv://chinedu:survey@survey-wizard.aqdd3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-//# sourceMappingURL=db.js.map
