13fdb679b430b24f9a42f8b3d12bd0162ee22820
JoabPa <joabparedes.ec@gmail.com>
2021-07-16 00:39:32 -0400

    explore page updated

---
+++ .history/Server/Routes/index_20210715235621.ts
@@ -0,0 +1,31 @@
+// modules required for routing
+import express from "express";
+const router = express.Router();
+export default router;
+
+// define the game model
+
+/* GET home page */
+router.get("/", (req, res, next) => {
+  res.render("../views/Content/index.ejs", {
+    title: "Home",
+  });
+});
+
+/*GET explore page*/
+router.get("/explorePage", (req, res, next) => {
+  res.render("../views/Explore/explore.ejs", {
+    title: "Home",
+  });
+});
+
+/*GET mySurveys page*/
+router.get("/mySurveys", (req, res, next) => {
+  res.render("../Views/mySurveys/mySurveys.ejs", {
+    title: "Home",
+  });
+});
+
+
+
+//module.exports = router;
