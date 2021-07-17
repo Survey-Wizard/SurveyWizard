8c917f38ace2c0770d9b01a51085ca88224d5a50
JoabPa <joabparedes.ec@gmail.com>
2021-07-16 21:55:20 -0400

    explore page updated to a table with buttons to different surveys. but crashes when trying to use the survey model

--- .history/Server/Models/survey_20210716205903.ts
+++ .history/Server/Models/survey_20210716205903.ts
@@ -3,21 +3,21 @@ const Schema = mongoose.Schema;
 
 const SurveySchema = new Schema
 ({
-  surveyName: { type: String, required: true },
+    surveyName: { type: String, required: true },
   surveyCategory: { type: String, required: true },
   publicValue: { type: String, required: true },
   surveyType: { type: String, required: true },
 
 
   questions: [String],
-  respones: {type: String},
-  active: Boolean,
-  lifeSpan: {
-      type: Date,
-      default: Date.now(),
-      expire: new Date(2021, 8, 20)
-  },
-  owner: String
+    respones: {type: String},
+    active: Boolean,
+    lifeSpan: {
+        type: Date,
+        default: Date.now(),
+        expire: new Date(2021, 8, 20)
+    },
+    owner: String
 },
 {
     collection: "survey"
